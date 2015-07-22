715e77c51b8cd64ce4f1f887123ae3214392a485
Aleksey Verkholantsev <poshli.pismo@gmail.com>
2015-07-22 23:29:26 +0300

    Incrementing and decrementing red letters number

--- index.js
+++ index.js
@@ -13,46 +13,13 @@ var convertHTML = require('html-to-vdom')({
 var currentVtree;
 var rootElement;
 
-document.addEventListener('DOMContentLoaded', function () {
-    var addElementButton = document.querySelector('.add-element');
-    addElementButton.addEventListener('click', handleAddElement);
-
-    var incrementIndexButton = document.querySelector('.increment-start-index');
-    incrementIndexButton.addEventListener('click', handleIncrementIndex);
-
-    var decrementIndexButton = document.querySelector('.decrement-start-index');
-    decrementIndexButton.addEventListener('click', handleDecrementIndex);
-
-    rootElement = document.querySelector('.root');
-    currentVtree = convertHTML(rootElement.innerHTML);
-});
-
-var state = {
-    length: 0,
-    startIndex: 0
-};
-
-function handleAddElement() {
-    state.length++;
-    render();
-}
-
-function handleIncrementIndex() {
-    state.startIndex++;
-    render();
-}
-
-function handleDecrementIndex() {
-    state.startIndex--;
-    render();
-}
-
 function render() {
     var bemjson = Array.apply(null, {length: state.length})
         .map(function (_, index) {
             return {
                 block: 'hello',
-                name: index + state.startIndex
+                name: String(index + state.startIndex),
+                redLettersNumber: state.redLettersNumber
             };
         });
 
@@ -64,4 +31,54 @@ function render() {
     rootElement = document.querySelector('.root');
     currentVtree = newVtree;
 
+}
+
+function renderDecorator(fn) {
+    return function () {
+        var result = fn.apply(this, arguments);
+        render();
+        return result;
+    };
+}
+
+var state = {
+    length: 0,
+    startIndex: 0,
+    redLettersNumber: 0
+};
+
+var handleAddElement = renderDecorator(function () {
+    state.length++;
+});
+
+var handleIncrementIndex = renderDecorator(function () {
+    state.startIndex++;
+});
+
+var handleDecrementIndex = renderDecorator(function () {
+    state.startIndex--;
+});
+
+var handleIncrementRedLetttersNumber = renderDecorator(function () {
+    state.redLettersNumber++;
+});
+
+var handleDecrementRedLetttersNumber = renderDecorator(function () {
+    state.redLettersNumber--;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    onClick('.add-element', handleAddElement);
+    onClick('.increment-start-index', handleIncrementIndex);
+    onClick('.decrement-start-index', handleDecrementIndex);
+    onClick('.increment-red-letters-number', handleIncrementRedLetttersNumber);
+    onClick('.decrement-red-letters-number', handleDecrementRedLetttersNumber);
+
+    rootElement = document.querySelector('.root');
+    currentVtree = convertHTML(rootElement.innerHTML);
+});
+
+function onClick(selector, handler) {
+    var node = document.querySelector(selector);
+    node.addEventListener('click', handler);
 }
--- template.bemhtml.js
+++ template.bemhtml.js
@@ -1,3 +1,25 @@
 block('hello').content()(function () {
-    return ['Hello, ', this.ctx.name, '!'];
+    var name = this.ctx.name;
+    var redLettersNumber = this.ctx.redLettersNumber;
+
+    return [
+        {
+            elem: 'first-part',
+            content: name.slice(0, redLettersNumber)
+        },
+        {
+            elem: 'second-part',
+            content: name.slice(redLettersNumber)
+        }
+    ];
 });
+
+block('hello').elem('first-part').attrs()(function () {
+    return {
+        style: 'color: red;'
+    };
+});
+
+block('hello').elem('first-part').tag()('span');
+
+block('hello').elem('second-part').tag()('span');
